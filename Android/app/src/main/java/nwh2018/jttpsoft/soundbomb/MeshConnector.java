2323fdeb1a633cda080d7edc5a48eeb9ec2113f1
Thomas <trichmond@vigiltechnologies.com>
2018-01-13 20:45:36 -0800

    Added rest of app

--- Android/app/src/main/java/nwh2018/jttpsoft/soundbomb/MeshConnector.java
+++ Android/app/src/main/java/nwh2018/jttpsoft/soundbomb/MeshConnector.java
@@ -1,31 +1,49 @@
-//package nwh2018.jttpsoft.soundbomb;
-//
-//import android.app.Activity;
-//
-//import java.util.HashSet;
-//
-///**
-// * Created by Thomas on 2018-01-13.
-// */
-//
-//public class MeshConnector implements MeshStateListener{
-//
-//    AndroidMeshManager meshManager = null;
-//    HashSet<MeshID> = new HashSet<>();
-//
-//    /**
-//     * Manages all current RightMesh connections.
-//     * @param srcActivity The activity from which the MeshManager is being used.
-//     */
-//    public MeshConnector(Activity srcActivity){
-//        meshManager = AndroidMeshManager.getInstance(srcActivity, this);
-//    }
-//
-//    public void resume() {
-//        meshManager.resume();
-//    }
-//
-//    public void close(){
-//        meshManager.stop();
-//    }
-//}
+package nwh2018.jttpsoft.soundbomb;
+
+import android.app.Activity;
+
+import java.util.HashSet;
+
+import io.left.rightmesh.android.AndroidMeshManager;
+import io.left.rightmesh.android.MeshService;
+import io.left.rightmesh.id.MeshID;
+import io.left.rightmesh.mesh.MeshStateListener;
+
+/**
+ * Created by Thomas on 2018-01-13.
+ */
+
+public class MeshConnector implements MeshStateListener {
+
+    AndroidMeshManager meshManager = null;
+    HashSet<MeshID> users = new HashSet<>();
+
+    /**
+     * Manages all current RightMesh connections.
+     * @param srcActivity The activity from which the MeshManager is being used.
+     */
+    public MeshConnector(Activity srcActivity){
+        meshManager = AndroidMeshManager.getInstance(srcActivity, this);
+    }
+
+    public void resume() {
+        try {
+            meshManager.resume();
+        } catch (MeshService.ServiceDisconnectedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close(){
+        try {
+            meshManager.stop();
+        } catch (MeshService.ServiceDisconnectedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void meshStateChanged(MeshID meshID, int i) {
+
+    }
+}
